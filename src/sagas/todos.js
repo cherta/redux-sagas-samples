30da02465c36af374f473ae0a1aa93df18032d4b
Chad Ostrowski <hi@chadoh.com>
2016-11-04 11:26:39 -0400

    Make it run; resolve warnings; log "network requests"

--- src/sagas/todos.js
+++ src/sagas/todos.js
@@ -6,16 +6,19 @@ import {
   COMPLETE, COMPLETE_SUCCESS, COMPLETE_FAIL,
 } from '../reducers/todos';
 
-function* load(action) {
+function* load(action) { // eslint-disable-line no-unused-vars
   const todos = yield fetch(`/todos?status=${action.filter}`);
 
-  yield put({
-    type: LOAD_SUCCESS,
-    items: todos,
-  })
+  try {
+    yield put({ type: LOAD_SUCCESS, items: todos })
+  }
+  catch (e) {
+    if (e instanceof Error) throw e;
+    yield put({ ...action, type: LOAD_FAIL, error: e.resource });
+  }
 }
 
-function* smartLoad(action) {
+function* smartLoad(action) { // eslint-disable-line no-unused-vars
   const isEverythingLoaded = yield select((state) =>
     Object.keys(state.todos.complete).length > 0 &&
     Object.keys(state.todos.incomplete).length > 0
@@ -24,23 +27,29 @@ function* smartLoad(action) {
 
   const todos = yield fetch(`/todos?status=${action.filter}`);
 
-  yield put({
-    type: LOAD_SUCCESS,
-    items: todos,
-  });
+  try {
+    yield put({ type: LOAD_SUCCESS, items: todos });
+  }
+  catch (e) {
+    if (e instanceof Error) throw e;
+    yield put({ ...action, type: LOAD_FAIL, error: e.resource });
+  }
 }
 
 function* complete(action) {
   const result = yield update(`/todos?id=${action.id}`);
-  yield put({
-    type: COMPLETE_SUCCESS,
-    item: result,
-  })
+  try {
+    yield put({ type: COMPLETE_SUCCESS, item: result })
+  }
+  catch (e) {
+    if (e instanceof Error) throw e;
+    yield put({ ...action, type: COMPLETE_FAIL, error: e.resource });
+  }
 }
 
 function* watchLoad() {
-  yield* takeLatest(LOAD, load);
-  // yield* takeLatest(LOAD, smartLoad);
+  // yield* takeLatest(LOAD, load);
+  yield* takeLatest(LOAD, smartLoad);
 }
 
 function* watchComplete() {
