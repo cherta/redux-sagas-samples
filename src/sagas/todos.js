0fab416c03a7cc8653072255855d64033abd1945
Gabriel Chertok <gchertok@gmail.com>
2016-11-04 10:45:11 -0300

    Demo 2 different sagas
    * One dumb saga that always loads
    * One smart saga that doesn't load if it's not needed
    * Some bug fixing on `fetch`

--- src/sagas/todos.js
+++ src/sagas/todos.js
@@ -1,11 +1,9 @@
 import { takeLatest, delay } from 'redux-saga';
-import { fork, put } from 'redux-saga/effects';
+import { fork, put, select } from 'redux-saga/effects';
 import { fetch } from '../services';
 import { LOAD, LOAD_SUCCESS, LOAD_FAIL } from '../reducers/todos';
 
 function* load(action) {
-  yield delay(1000);
-
   const todos = yield fetch(`/todos?status=${action.filter}`);
 
   yield put({
@@ -15,8 +13,25 @@ function* load(action) {
 
 }
 
+function* smartLoad(action) {
+  const isEverythingLoaded = yield select((state) =>
+    Object.keys(state.todos.complete).length > 0 &&
+    Object.keys(state.todos.incomplete).length > 0
+  );
+  if (isEverythingLoaded) return;
+
+  const todos = yield fetch(`/todos?status=${action.filter}`);
+
+  yield put({
+    type: LOAD_SUCCESS,
+    items: todos,
+  });
+
+}
+
 function* watchLoad() {
   yield* takeLatest(LOAD, load);
+  // yield* takeLatest(LOAD, smartLoad);
 }
 
 export default function* watchTodos() {
--- src/services/index.js
+++ src/services/index.js
@@ -1,16 +1,20 @@
 export const fetch = (url) => {
   return new Promise((resolve, reject) => {
     if (url === '/todos?status=complete') {
-      setTimeout(1000, resolve([
-        { id: 1, text: 'learn about JSON', status: 'complete' },
-        { id: 2, text: 'learn about redux', status: 'complete' },
-      ]));
+      setTimeout(() => {
+        resolve([
+          { id: 1, text: 'learn about JSON', status: 'complete' },
+          { id: 2, text: 'learn about redux', status: 'complete' },
+        ]);
+      }, 1000);
     } else {
-      setTimeout(1000, resolve([
-        { id: 1, text: 'learn about JSON', status: 'complete' },
-        { id: 2, text: 'learn about redux', status: 'complete' },
-        { id: 3, text: 'learn about sagas', status: 'incomplete' },
-      ]));
+      setTimeout(() => {
+        resolve([
+          { id: 1, text: 'learn about JSON', status: 'complete' },
+          { id: 2, text: 'learn about redux', status: 'complete' },
+          { id: 3, text: 'learn about sagas', status: 'incomplete' },
+        ]);
+      }, 1000);
     }
   });
 }
